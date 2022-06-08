4fa6acd425b3639dc9ea292e2a301476031f722e
morrellj <morrellj.n7@gmail.com>
2022-06-08 23:13:34 +0800

    many changes later

--- assessment.js
+++ assessment.js
@@ -7,8 +7,20 @@ let client = {};
 
 elements = new Elements(Client.schema);
 
+var header = document.getElementById("buttonNav");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    if (current[0]) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
+
 function setPage(heading) {
-  var child = root.lastElementChild;
+  let child = root.lastElementChild;
   while (child) {
     root.removeChild(child);
     child = root.lastElementChild;
@@ -37,6 +49,14 @@ function clientAdd() {
   addNewClientSelectOption(client.key, client.firstName, client.surname);
   firstNameField.value = "";
   surnameNameField.value = "";
+  clientSelectAction(client.key);
+  let options = clientListSelect.options;
+  let opt;
+  for (let i = 0, iLen = options.length; i < iLen; i++) {
+    opt = options[i];
+    opt.selected = opt.value == client.key ? true : false;
+  }
+  toggle(document.getElementById("newClientAddForm"));
 }
 function addNewClientSelectOption(thisKey, thisFirstName, thisSurname) {
   let newOption = document.createElement("option");
@@ -45,8 +65,9 @@ function addNewClientSelectOption(thisKey, thisFirstName, thisSurname) {
   clientListSelect.appendChild(newOption);
 }
 
-function clientSelectAction() {
-  Client.getClient(clientListSelect.value, function (data) {
+function clientSelectAction(clientKey) {
+  Client.getClient(clientKey, function (data) {
+    //clientListSelect.value
     output.innerHTML = data.firstName + " " + data.surname;
     elements.updateElements(data);
   });
@@ -56,8 +77,77 @@ function clientDelete() {
   alert("Open developer tools and delete manually from local storage.");
 }
 function refreshState() {
-  var all = document.getElementsByTagName("input");
-  for (var i = 0, max = all.length; i < max; i++) {
+  let all = document.getElementsByTagName("input");
+  for (let i = 0, max = all.length; i < max; i++) {
     all[i].value = "x";
+  }
+}
+function toggle(element) {
+  if (element.hidden == false) {
+    element.hidden = true;
+  } else {
+    element.hidden = false;
+  }
+}
+
+function getAllStorage() {
+  let values = [],
+    keys = Object.keys(localStorage),
+    i = keys.length;
+
+  while (i--) {
+    let client = JSON.parse(localStorage.getItem(keys[i]));
+    client.key = keys[i];
+    values.push(client);
+  }
+
+  return values;
+}
+function csv() {
+  let allClients = getAllStorage();
+  let fields = Object.keys(Client.schema);
+  fields.unshift("key");
+  let replacer = function (value) {
+    return value ? value : "";
+  };
+  let csv = allClients.map(function (row) {
+    return fields
+      .map(function (fieldName) {
+        let thisString = "";
+        if (Array.isArray(row[fieldName])) {
+          row[fieldName].forEach((element, index) => {
+            let comma = index > 0 ? ", " : "";
+            thisString = thisString + comma + element;
+          });
+        } else {
+          thisString = row[fieldName];
+        }
+        return JSON.stringify(replacer(thisString));
+      })
+      .join(",");
+  });
+  csv.unshift(fields.join(","));
+  csv = csv.join("\r\n");
+  console.log(csv);
+  return csv;
+}
+function writeToCSVfile(csv, name) {
+  let blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+  if (navigator.msSaveBlob) {
+    // IE 10+
+    navigator.msSaveBlob(blob, name);
+  } else {
+    let link = document.createElement("a");
+    if (link.download !== undefined) {
+      // feature detection
+      // Browsers that support HTML5 download attribute
+      let url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", name);
+      link.style.visibility = "hidden";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   }
 }
--- modelElements.js
+++ modelElements.js
@@ -1,15 +1,26 @@
 class Elements {
   constructor(schema) {
     this.array = [];
+
     for (const [dataField, value] of Object.entries(schema)) {
       //create tag
       this[dataField] = document.createElement(value.tag);
+      //create div and label
+      let inputDiv = document.createElement("div");
+      inputDiv.id = dataField + "Div";
+      inputDiv.classList.add("flex-item");
+      let inputLabel = document.createElement("p");
+      inputLabel.innerHTML = value.label;
+      //append element and label to div
+      inputDiv.appendChild(inputLabel);
+      inputDiv.appendChild(this[dataField]);
       //add ID
       this[dataField].id = dataField;
       //add class list
       value.classes.forEach((ele) => {
         this[dataField].classList.add(ele);
       });
+
       //add on input funtion to update client record as user types
       this[dataField].oninput = function () {
         let changeObject = {};
@@ -27,15 +38,23 @@ class Elements {
       if (value.type) {
         this[dataField].type = value.type;
       }
+      // add color
+      value.classes.forEach((element) => {
+        if (Client.colors[element]) {
+          this[dataField].style.backgroundColor = Client.colors[element];
+        }
+      });
       //unique actions for specific tag types
       switch (value.tag) {
         case "select":
-          let blankOption = document.createElement("option");
-          blankOption.value = "";
-          blankOption.innerHTML = "";
-          blankOption.hidden = true;
-          blankOption.selected = "";
-          this[dataField].appendChild(blankOption);
+          if (!value.multiple) {
+            let blankOption = document.createElement("option");
+            blankOption.value = "";
+            blankOption.innerHTML = "";
+            blankOption.hidden = true;
+            blankOption.selected = "";
+            this[dataField].appendChild(blankOption);
+          }
           value.selectOptions.forEach((option) => {
             let selectOption = document.createElement("option");
             selectOption.value = option;
@@ -60,45 +79,65 @@ class Elements {
         //     break;
         default:
       }
-      //create div and label
-      let inputDiv = document.createElement("div");
-      inputDiv.id = dataField + "Div";
-      let inputLabel = document.createElement("p");
-      inputLabel.innerHTML = value.label;
-      //append element and label to div
-      inputDiv.appendChild(inputLabel);
-      inputDiv.appendChild(this[dataField]);
+
       //push div to array
       this.array.push(inputDiv);
     }
     //class functions
     this.addSpecifiedElementsToTargetDiv = function (tag, targetDiv) {
+      let count = 0;
+      let nextDiv;
+      function subRootOneDiv() {
+        let tempDiv = document.createElement("div");
+        tempDiv.id = "subroot-" + count / 3;
+        tempDiv.classList.add("flex-item");
+        tempDiv.classList.add("sub-root-1");
+        return tempDiv;
+      }
+      nextDiv = subRootOneDiv();
+      root.appendChild(nextDiv);
       this.array.forEach(function (element) {
-        if (tag == "All") {
-          targetDiv.appendChild(element);
-        } else if (element.lastChild.classList.contains(tag)) {
-          targetDiv.appendChild(element);
+        addNextElement(tag, nextDiv);
+        function addNextElement(tag, target) {
+          if (tag == "All") {
+            target.appendChild(element);
+            count += 1;
+          } else if (element.lastChild.classList.contains(tag)) {
+            target.appendChild(element);
+            count += 1;
+          }
+          if (count % 3 == 0 && count > 0) {
+            nextDiv = subRootOneDiv();
+            root.appendChild(nextDiv);
+          }
         }
       });
+      while (count % 3 != 0) {
+        nextDiv = document.createElement("div");
+        nextDiv.classList.add("flex-item");
+        nextDiv.classList.add("blank-fill");
+        root.lastChild.appendChild(nextDiv);
+        count += 1;
+      }
     };
     this.updateElements = function (data) {
       this.array.forEach((ele) => {
         let dataField = ele.lastChild;
         if (data[dataField.id]) {
-          dataField.value = data[dataField.id];
+          if (dataField.tagName == "SELECT" && dataField.multiple == true) {
+            setMultiSelectValues(dataField, data);
+          } else {
+            dataField.value = data[dataField.id];
+          }
         } else {
-          // if (dataField.tagName == "SELECT") {
-          //   dataField.value = Client.schema[dataField.id].selected;
-          // } else {
           dataField.value = "";
-          // }
         }
         dataField.name = data.key;
       });
     };
     this.getMultiSelectValues = function (select) {
       var result = [];
-      var options = select && select.options;
+      var options = select.options;
       var opt;
 
       for (var i = 0, iLen = options.length; i < iLen; i++) {
@@ -110,6 +149,31 @@ class Elements {
       }
       return result;
     };
-    function setMultiSelectValues() {}
+    function setMultiSelectValues(element, clientData) {
+      var options = element.options;
+      var opt;
+      for (var i = 0, iLen = options.length; i < iLen; i++) {
+        options[i].selected = false;
+      }
+      if (clientData[element.id]) {
+        if (Array.isArray(clientData[element.id])) {
+          clientData[element.id].map((value) => {
+            for (var i = 0, iLen = options.length; i < iLen; i++) {
+              opt = options[i];
+              if (opt.value == value) {
+                opt.selected = true;
+              }
+            }
+          });
+        } else if (typeof clientData[element.id] === "string") {
+          for (var i = 0, iLen = options.length; i < iLen; i++) {
+            opt = options[i];
+            if (opt.value == clientData[element.id]) {
+              opt.selected = true;
+            }
+          }
+        }
+      }
+    }
   }
 }
