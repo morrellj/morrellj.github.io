15991063190400a029a94974019e0fd1d38b94be
morrellj <morrellj.n7@gmail.com>
2023-01-03 21:07:22 +0800

    removed redundant code

--- components/singleLikertField.js
+++ components/singleLikertField.js
@@ -37,40 +37,4 @@ class SingleLikertField extends Builder {
     };
     self.manufacture(self.schema);
   }
-  dataFieldOnInput = function () {
-    let changeObject = {};
-    if (store.state.activeRecord) {
-      changeObject[this.name] =
-        this.tagName == "SELECT"
-          ? elements.getMultiSelectValues(this)
-          : this.value;
-      store.dispatch("update", {
-        id: store.state.activeRecord,
-        data: changeObject,
-      });
-    } else {
-      alert("Select a client");
-    }
-  };
-  dataFieldOncontextmenuPopUpPop = function (event) {
-    if (
-      store.state.records[store.state.activeRecord][this.name].constructor !=
-      Object
-    )
-      return;
-    event.preventDefault();
-    removePopUpContent();
-    popUp.style.display = "block";
-    let paragraphDiv = document.createElement("p");
-    let paragraph = Array.isArray(
-      store.state.records[store.state.activeRecord][this.name].previous
-    )
-      ? store.state.records[store.state.activeRecord][this.name].previous.join(
-          ", "
-        )
-      : store.state.records[store.state.activeRecord][this.name].previous;
-
-    paragraphDiv.innerHTML = paragraph;
-    popUpContent.appendChild(paragraphDiv);
-  };
 }
