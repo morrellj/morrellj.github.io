bee5dcebd593c14213042fdfa5babd8bcedba07b
Jon Morrell <morrellj.n7@gmail.com>
2025-09-14 21:07:13 +0800

    removed old git io files

--- appRecords.js
+++
@@ -1,6 +0,0 @@
-(function (app) {
-  app.recordsActions = {};
-  app.recordsActions.delete = () => {
-    alert("Open developer tools and delete manually from local storage.");
-  };
-})(app);
--- components/dataFieldVariations.js
+++
@@ -1,8 +0,0 @@
-let dataFieldVariations = {
-  select(schema, fieldSettings) {
-    schema.$_dataField.props.multiple = fieldSettings.multiple;
-    schema.$_dataField.props.size = "6";
-    schema.$_dataField.children = new SelectFieldOptions(fieldSettings).schema;
-    return schema;
-  },
-};
